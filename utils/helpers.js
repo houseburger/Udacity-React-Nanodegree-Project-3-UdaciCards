d42d3386f3889c0b213ddf4f1a93eb62261c5b86
Pasci <hausberger.pascal@gmail.com>
2019-02-08 16:25:03 +0100

    feat: AsyncStorage helper methods
    added Lodash, React Native, API helper methods using AsyncStorage

---
+++ utils/helpers.js
@@ -0,0 +1,35 @@
+import { AsyncStorage } from 'react-native'
+import _ from 'lodash'
+
+const STORAGE_KEY = 'UdaciCards:decks'
+
+export function getDecks() {
+  return AsyncStorage.getItem(STORAGE_KEY)
+}
+
+export function getDeck(id) {
+  return AsyncStorage.getItem(STORAGE_KEY)
+    .then((data) => {
+      const decks = JSON.parse(data)
+      return _.get(decks, id) // or use _.pick (which would then create object and insert needed object inside that object). _.get might be more direct!
+    })
+}
+
+export function saveDeckTitle(title) {
+  return AsyncStorage.getItem(STORAGE_KEY)
+    .then((data) => {
+      const decks = JSON.parse(data)
+      let deck = { title: title}
+      let newData = { ...decks,  ...deck }
+      AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(newData))
+    })
+}
+
+export function addCardToDeck(title, card) {
+  return AsyncStorage.getItem(STORAGE_KEY)
+    .then((data) => {
+      let decks = JSON.parse(data)
+      decks[title] = { ...decks[title], questions: card }
+      AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(decks))
+    })
+}
